8000e5d16ebba501b34eaf4be0d57134120257b9
Pablo Betancor <pablojose.betancor@autentia.com>
2019-01-11 12:50:00 +0100

    Ejemplo de naive bayes con big data

---
+++ src/main/java/com/autentia/wekaexamples/mains/BigData.java
@@ -0,0 +1,96 @@
+package com.autentia.wekaexamples.mains;
+
+import com.autentia.wekaexamples.utils.DataSourceUtils;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+import java.io.File;
+
+/**
+ * <p>Recibe el fichero de entrenamiento y de tests como parámetros. Si no existe los crea con {@link weka.datagenerators.classifiers.classification.LED24}
+ * con tamaño de medio giga para el de entrenamiento y de medio mega para el de testeo.</p>
+ *
+ * <p>Se ha comprobado que con estos tamaños J48 al no ser incremental falla. También se ha comprobado que también falla
+ * el cross-validation por lo mismo, la validación debe ser con fichero de test a parte.</p>
+ *
+ */
+public class BigData {
+
+    public static void main(String [] args) throws Exception {
+
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Se deben de pasar dos parámetros, uno con el path del fichero de " +
+                    "entrenamiento y otro con el path del fichero de tests. Si los ficheros no existen se crearán con " +
+                    "el generador LED24 y tamaños ??¿¿?");
+        }
+
+        String trainFilePath = args[0];
+        String testFilePath = args[1];
+
+        File trainFile = getTrainFile(trainFilePath);
+        File testFile = getTestFile(testFilePath);
+
+        DataSourceUtils dataSourceUtils = new DataSourceUtils();
+        dataSourceUtils.newWekaInstances(trainFile);
+
+        System.out.println("Cargadas las instancias del fichero de entrenamiento y testeo.");
+
+        ArffLoader loader = new ArffLoader();
+        loader.setFile(trainFile);
+        Instances trainInstances = loader.getStructure();
+        trainInstances.setClassIndex(trainInstances.numAttributes() - 1);
+
+        NaiveBayesUpdateable naiveBayesUpdatable = new NaiveBayesUpdateable();
+        naiveBayesUpdatable.buildClassifier(trainInstances);
+
+        System.out.println("Comenzamos a entrenar el modelo.");
+
+        Instance instance;
+
+        while ((instance = loader.getNextInstance(trainInstances))  != null) {
+            naiveBayesUpdatable.updateClassifier(instance);
+        }
+
+        System.out.println("\nComenzamos a testear el modelo.");
+
+        Instances testInstances = dataSourceUtils.newWekaInstances(testFile);
+        Evaluation evaluation = new Evaluation(testInstances);
+
+        evaluation.evaluateModel(naiveBayesUpdatable, testInstances);
+
+        boolean printComplexityStatistics = false;
+        System.out.println(evaluation.toSummaryString(printComplexityStatistics));
+
+        System.out.println(evaluation.toMatrixString());
+    }
+
+    private static File getTrainFile(String trainFilePath) throws Exception {
+
+        File trainFile = new File(trainFilePath);
+
+        if (!trainFile.exists()) {
+            System.out.println("No existe el fichero de entrenamiento [" + trainFilePath + "]. " +
+                    "Se autogenerará uno con 10 millones de instancias.");
+            trainFile = DataSourceUtils.createLed24DataSource(trainFilePath, 10000000);
+        }
+
+        return trainFile;
+    }
+
+    private static File getTestFile(String testFilePath) throws Exception {
+
+        File testFile = new File(testFilePath);
+
+        if (!testFile.exists()) {
+            System.out.println("No existe el fichero de testeo [" + testFilePath + "]. " +
+                    "Se autogenerará uno con 10 mil instancias.");
+            testFile = DataSourceUtils.createLed24DataSource(testFilePath, 10000);
+        }
+
+        return testFile;
+    }
+
+}
--- src/main/java/com/autentia/wekaexamples/utils/DataSourceUtils.java
+++ src/main/java/com/autentia/wekaexamples/utils/DataSourceUtils.java
@@ -1,11 +1,26 @@
 package com.autentia.wekaexamples.utils;
 
 import weka.core.Instances;
+import weka.core.Utils;
 import weka.core.converters.ConverterUtils;
+import weka.datagenerators.classifiers.classification.LED24;
 
 import java.io.File;
 
 public class DataSourceUtils {
+
+    public static File createLed24DataSource(String path, int numExamples) throws Exception {
+
+        String optionsStr = new StringBuilder("-o ").append(path)
+                .append(" -n ").append(numExamples)
+                .toString();
+
+        String[] options = Utils.splitOptions(optionsStr);
+
+        LED24.runDataGenerator(new LED24(), options);
+
+        return new File(path);
+    }
 
     public Instances newWekaInstances(File file) throws Exception {
         ConverterUtils.DataSource source = new ConverterUtils.DataSource(file.getAbsolutePath());
@@ -13,4 +28,5 @@ public class DataSourceUtils {
         instances.setClassIndex(instances.numAttributes() - 1);
         return instances;
     }
+
 }
